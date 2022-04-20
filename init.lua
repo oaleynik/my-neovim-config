e1104e05a8690ebdc8b5e22e2abb0c7446839ca9
Oleh Aleinyk <oleg.aleynik@gmail.com>
2022-04-20 15:29:45 +0300

    Setup LSP servers using recommended configuration

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ require 'user.settings'
 require 'user.keymap'
 require 'user.packer'
 
+require 'user.lspconfig'
 require 'user.nvim-tree'
 require 'user.nvim-lualine'
 require 'user.treesitter'
--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -16,6 +16,7 @@ return require('packer').startup(function()
   use 'voldikss/vim-floaterm'
   use 'windwp/nvim-autopairs'
   use 'windwp/nvim-ts-autotag'
+  use 'neovim/nvim-lspconfig'
 
   -- NvimTree
   use 'kyazdani42/nvim-tree.lua'
