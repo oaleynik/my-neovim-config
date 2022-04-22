038130ea9d449505f955c62e81040b0095a616c5
Oleh Aleinyk <oleg.aleynik@gmail.com>
2022-04-23 01:51:02 +0300

    Add eslint config for typescript fixture

--- fixtures/typescript/main.ts
+++ fixtures/typescript/main.ts
@@ -1,8 +1,8 @@
+import { sum } from './helper'
+
+const result = sum(1, 2)
+console.log(result)
+
 const mySuperLongStirngVariableName = 'Hello world'
 
 console.log(mySuperLongStirngVariableName)
-
-import { sum } from './helper'
-
-let result = sum(1, 2)
-console.log(result)
