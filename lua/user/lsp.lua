e6870f6b77cc14709705cac41fea8d8be596d2ab
Oleh Aleinyk <oleg.aleynik@gmail.com>
2022-04-23 00:25:54 +0300

    Add volar LSP config

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -94,7 +94,6 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- Example custom server
 -- Make runtime files discoverable to the server
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
@@ -128,3 +127,26 @@ lspconfig.sumneko_lua.setup {
   },
 }
 
+local util = require('lspconfig.util')
+
+local function get_typescript_server_path(root_dir)
+  local project_root = util.find_node_modules_ancestor(root_dir)
+
+  local local_tsserverlib = project_root ~= nil and util.path.join(project_root, 'node_modules', 'typescript', 'lib', 'tsserverlibrary.js')
+  local global_tsserverlib = os.getenv('NVIM_LSP_TSSERVER_PATH')
+
+  if local_tsserverlib and util.path.exists(local_tsserverlib) then
+    return local_tsserverlib
+  else
+    return global_tsserverlib
+  end
+end
+
+lspconfig.volar.setup {
+  -- filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' },
+  on_attach = on_attach,
+  capabilities = capabilities,
+  on_new_config = function(new_config, new_root_dir)
+    new_config.init_options.typescript.serverPath = get_typescript_server_path(new_root_dir)
+  end,
+}
