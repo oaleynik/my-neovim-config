9c75d3a4f91d832f2e1049e7de80de93689256bf
Oleh Aleinyk <oleg.aleynik@gmail.com>
2022-04-22 21:26:42 +0300

    Enable html, css, json language servers

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -95,7 +95,7 @@ local on_attach = function(_, bufnr)
 end
 
 -- Enable the following language servers
-local servers = { 'tsserver', 'gopls' }
+local servers = { 'tsserver', 'gopls', 'html', 'cssls', 'jsonls' }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
