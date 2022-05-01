399677a5b48b85cf2d89b5102e40e2d860b8025e
Oleh Aleinyk <oleg.aleynik@gmail.com>
2022-05-01 14:26:57 +0300

    Disable spell check completion

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -156,6 +156,5 @@ require('null-ls').setup({
   sources = {
     require('null-ls').builtins.formatting.stylua,
     require('null-ls').builtins.diagnostics.eslint,
-    require('null-ls').builtins.completion.spell,
   },
 })
