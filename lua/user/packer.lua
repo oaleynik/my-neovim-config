bbc12fea6218ff96137f25cb580c72d312f8e913
Oleh Aleinyk <oleg.aleynik@gmail.com>
2022-07-27 11:48:51 +0300

    Formatting changes

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -34,8 +34,8 @@ require('packer').startup(function(use)
   use 'nvim-treesitter/nvim-treesitter-textobjects'
 
   -- Autopairs
-  use { 'windwp/nvim-autopairs', config = function() require('nvim-autopairs').setup{} end }
-  use { 'windwp/nvim-ts-autotag', config = function() require('nvim-ts-autotag').setup{} end }
+  use { 'windwp/nvim-autopairs', config = function() require('nvim-autopairs').setup {} end }
+  use { 'windwp/nvim-ts-autotag', config = function() require('nvim-ts-autotag').setup {} end }
 
   -- Telescope
   use 'nvim-telescope/telescope.nvim'
