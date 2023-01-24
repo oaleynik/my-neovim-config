85f8370f96d7a7db2d3963ed44a9e408894a01ca
Oleh Aleinyk <oleg.aleynik@gmail.com>
2023-01-24 14:02:29 +0200

    gitsigns: ]h and [h to move between hunks

--- after/plugin/gitsigns.rc.lua
+++ after/plugin/gitsigns.rc.lua
@@ -1,4 +1,12 @@
 local status, gitsigns = pcall(require, 'gitsigns')
 if (not status) then return end
 
-gitsigns.setup {}
+gitsigns.setup {
+  word_diff = true,
+  on_attach = function(bufnr)
+    local opts = { noremap = true, silent = true }
+
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', ']h', '<cmd>lua require"gitsigns".next_hunk()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '[h', '<cmd>lua require"gitsigns".prev_hunk()<CR>', opts)
+  end
+}
