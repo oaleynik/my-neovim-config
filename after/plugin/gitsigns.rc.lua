6f522ff5701caf232ecfe2ac0db4a70ff7441b33
Oleh Aleinyk <oleg.aleynik@gmail.com>
2023-02-03 17:05:28 +0200

    change: disable gitsigns word_diff

--- after/plugin/gitsigns.rc.lua
+++ after/plugin/gitsigns.rc.lua
@@ -2,7 +2,7 @@ local status, gitsigns = pcall(require, 'gitsigns')
 if (not status) then return end
 
 gitsigns.setup {
-  word_diff = true,
+  -- word_diff = true,
   on_attach = function(bufnr)
     local opts = { noremap = true, silent = true }
 
