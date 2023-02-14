dd4302fa276ff308ea8f0d379e9fff3f57c51c2b
Oleh Aleinyk <oleg.aleynik@gmail.com>
2023-02-14 11:45:15 +0200

    theme: setup noirbuddy

--- after/plugin/theme.rc.lua
+++ after/plugin/theme.rc.lua
@@ -1,5 +1,22 @@
 -- Theme
-vim.o.background = 'light'
+vim.o.background = 'dark'
 
-vim.cmd [[colorscheme onehalflight]]
+-- vim.cmd [[colorscheme kanagawa]]
 
+require('noirbuddy').setup {
+  -- preset = 'minimal',
+  -- preset = 'miami-nights',
+  preset = 'kiwi',
+  -- preset = 'slate',
+  -- preset = 'crt-green',
+  -- preset = 'crt-amber',
+  styles = {
+    italic = true,
+    -- bold = true,
+    -- underline = true,
+    -- undercurl = true,
+  },
+  colors = {
+    background = '#161616',
+  },
+}
