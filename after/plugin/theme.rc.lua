9ae4260cb22706d251d108a293a84e2bf7746f4c
Oleh Aleinyk <oleg.aleynik@gmail.com>
2023-04-27 12:30:18 +0300

    theme: set github light high contrast

--- after/plugin/theme.rc.lua
+++ after/plugin/theme.rc.lua
@@ -2,8 +2,8 @@
 vim.g.gruvbox_material_background = 'soft' -- medium, hard
 vim.g.gruvbox_material_better_performance = 1
 
-vim.o.background = 'dark'
-vim.cmd [[colorscheme gruvbox-material]]
+vim.o.background = 'light'
+vim.cmd [[colorscheme github_light_high_contrast]]
 
 -- require('noirbuddy').setup {
 --   -- preset = 'minimal',
