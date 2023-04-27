3112caaa1ed67e89bef39a5f62ff4a1901a7e411
Oleh Aleinyk <oleg.aleynik@gmail.com>
2023-04-27 12:29:17 +0300

    settings: enable shiftround

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -47,6 +47,7 @@ vim.o.autoindent = true
 vim.o.smartindent = true
 vim.o.tabstop = 2 -- 1 tab = 2 spaces
 vim.o.shiftwidth = 2 -- indent rule
+vim.o.shiftround = true
 vim.o.formatoptions = 'qnj1' -- q - comment formatting; n - numbered lists; j - remove comment when joining lines; 1 - don't break after one-letter word
 vim.o.expandtab = true -- expand tab to spaces
 vim.o.virtualedit = 'onemore'
