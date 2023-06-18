07a15b5ee68f300836d3ef6f8d59373dd5e16bc2
Oleh Aleinyk <oleg.aleynik@gmail.com>
2023-06-18 17:38:41 +0300

    lsp: drop omnifunc setting required for older version of neovim

--- after/plugin/lspconfig.rc.lua
+++ after/plugin/lspconfig.rc.lua
@@ -61,9 +61,6 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- Mappings.
   local opts = { noremap = true, silent = true }
