34ca62962fc80ec529d293c18e9f6e6761491ea0
Oleh Aleinyk <oleg.aleynik@gmail.com>
2023-08-14 12:41:09 +0300

    lspsaga: disable extra bulb sign on the left

--- after/plugin/lspsaga.rc.lua
+++ after/plugin/lspsaga.rc.lua
@@ -1,7 +1,12 @@
 local status, lspsaga = pcall(require, 'lspsaga')
 if (not status) then return end
 
-lspsaga.setup {}
+lspsaga.setup {
+  lightbulb = {
+    sign = false,
+    debounce = 100,
+  },
+}
 
 local keymap = vim.keymap.set
 
