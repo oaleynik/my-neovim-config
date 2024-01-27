a3178d22a8b0edce67a8083e858dec510f6053c8
Oleh Aleinyk <oleg.aleynik@gmail.com>
2024-01-27 19:20:11 +0200

    lazy: update plugins and install todo-comments

--- lua/oaleinyk/lazy/basics.lua
+++ lua/oaleinyk/lazy/basics.lua
@@ -10,6 +10,18 @@ return {
   'tpope/vim-unimpaired',
 
   {
+    'folke/todo-comments.nvim',
+
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+
+    config = function ()
+      require('todo-comments').setup({})
+    end
+  },
+
+  {
     'kyazdani42/nvim-web-devicons',
 
     config = function ()
