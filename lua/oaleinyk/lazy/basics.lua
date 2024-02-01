01573ed9e1bbccc661a9be8f504de92babf68a50
Oleh Aleinyk <oleg.aleynik@gmail.com>
2024-02-01 23:58:14 +0200

    lazy: add JoosepAlviste/nvim-ts-context-commentstring

--- lua/oaleinyk/lazy/basics.lua
+++ lua/oaleinyk/lazy/basics.lua
@@ -43,6 +43,10 @@ return {
   {
     'numToStr/Comment.nvim',
 
+    dependencies = {
+      'JoosepAlviste/nvim-ts-context-commentstring',
+    },
+
     config = function ()
       require('Comment').setup()
     end
