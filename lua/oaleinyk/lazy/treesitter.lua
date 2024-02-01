b7b7cc581460d27680fa05a33c7609cd7ccbe0b8
Oleh Aleinyk <oleg.aleynik@gmail.com>
2024-02-01 23:57:30 +0200

    treesitter: use minimum viable configuration

--- lua/oaleinyk/lazy/treesitter.lua
+++ lua/oaleinyk/lazy/treesitter.lua
@@ -9,75 +9,55 @@ return {
   build = ':TSUpdate',
 
   config = function ()
-    local status, ts = pcall(require, 'nvim-treesitter.configs')
-    if not status then
-      return
-    end
+    local treesitter = require('nvim-treesitter.configs')
 
-    ts.setup({
-      ensure_installed = 'all',
-      ignore_install = { 'phpdoc' },
-      sync_install = false,
-      autopairs = {
+    treesitter.setup({
+      ensure_installed = {
+        -- 'comment', -- comments are slowing down TS bigtime, so disable for now
+        'css',
+        'gitcommit',
+        'gitignore',
+        'go',
+        'html',
+        'http',
+        'javascript',
+        'lua',
+        'markdown',
+        'nix',
+        'rust',
+        'scss',
+        'sql',
+        'typescript',
+        'vimdoc',
+        'vue',
+      },
+      auto_install = true,
+      autopath = {
         enable = true,
       },
       highlight = {
         enable = true,
-        additional_vim_regex_highlighting = true,
       },
       indent = {
         enable = true,
-        disable = { 'yaml' },
-      },
-      context_commentstring = {
-        enable = true,
-        enable_autocmd = false,
       },
       incremental_selection = {
         enable = true,
-        keymaps = {
-          init_selection = 'gnn',
-          node_incremental = 'grn',
-          scope_incrememntal = 'grc',
-          node_decremental = 'grm',
-        },
+      },
+      playground = {
+        enable = true,
       },
       textobjects = {
         select = {
           enable = true,
-          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-          keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@class.outer',
-            ['ic'] = '@class.inner',
-          },
+        },
+        swap = {
+          enable = true,
         },
         move = {
           enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            [']m'] = '@function.outer',
-            [']]'] = '@class.outer',
-          },
-          goto_next_end = {
-            [']M'] = '@function.outer',
-            [']['] = '@class.outer',
-          },
-          goto_previous_start = {
-            ['[m'] = '@function.outer',
-            ['[['] = '@class.outer',
-          },
-          goto_previous_end = {
-            ['[M'] = '@function.outer',
-            ['[]'] = '@class.outer',
-          },
         },
       },
-      playground = {
-        enable = true,
-      }
     })
   end
 }
