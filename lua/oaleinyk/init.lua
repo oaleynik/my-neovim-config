43b0114c9bf97096d717534cbcaa8666d85e2783
Oleh Aleinyk <oleg.aleynik@gmail.com>
2024-04-25 13:43:43 +0300

    theme: tokyonight moon

--- lua/oaleinyk/init.lua
+++ lua/oaleinyk/init.lua
@@ -23,11 +23,11 @@ require('lazy').setup({
 
 vim.o.background = 'dark'
 
-vim.cmd('colorscheme gruvbox-material')
+vim.cmd('colorscheme tokyonight-moon')
+-- vim.cmd('colorscheme gruvbox-material')
 -- vim.cmd('colorscheme dayfox')
 -- vim.cmd('colorscheme catppuccin-latte')
 -- vim.cmd('colorscheme rose-pine')
--- vim.cmd('colorscheme tokyonight-night')
 -- vim.cmd('colorscheme melange')
 -- vim.cmd('colorscheme kanagawa')
 -- vim.cmd('colorscheme neobones')
--- lua/oaleinyk/lazy/themes.lua
+++ lua/oaleinyk/lazy/themes.lua
@@ -1,7 +1,8 @@
 return {
   {
     'folke/tokyonight.nvim',
-    lazy = true,
+    lazy = false,
+    priority = 1000,
   },
 
   {
@@ -68,8 +69,7 @@ return {
   {
     'sainnhe/gruvbox-material',
 
-    lazy = false,
-    priority = 1000,
+    lazy = true,
     config = function ()
       vim.g.gruvbox_material_background = 'hard' -- medium, hard
       vim.g.gruvbox_material_better_performance = 1
