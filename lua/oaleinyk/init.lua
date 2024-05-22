6c541372cc40803607125b3eb8fc1096fcab65ab
Oleh Aleinyk <oleg.aleynik@gmail.com>
2024-05-22 23:19:00 +0300

    theme: use tokyonight

--- lua/oaleinyk/init.lua
+++ lua/oaleinyk/init.lua
@@ -23,11 +23,11 @@ require("lazy").setup({
 
 vim.o.background = "dark"
 
-vim.cmd("colorscheme gruvbox-material")
+vim.cmd("colorscheme tokyonight-night")
+-- vim.cmd("colorscheme gruvbox-material")
 -- vim.cmd('colorscheme rose-pine')
 -- vim.cmd('colorscheme carbonfox')
 -- vim.cmd('colorscheme github_dark_default')
--- vim.cmd('colorscheme tokyonight-moon')
 -- vim.cmd('colorscheme catppuccin-latte')
 -- vim.cmd('colorscheme melange')
 -- vim.cmd('colorscheme kanagawa')
