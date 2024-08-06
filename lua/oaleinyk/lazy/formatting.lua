46d4835d9be39b60923a30d88a0a629811b9ad04
Oleh Aleinyk <oleg.aleynik@gmail.com>
2024-08-06 14:37:43 +0300

    lazy: update formatting settings

--- lua/oaleinyk/lazy/formatting.lua
+++ lua/oaleinyk/lazy/formatting.lua
@@ -7,7 +7,14 @@ return {
 			-- Customize or remove this keymap to your liking
 			"<leader>mp",
 			function()
-				require("conform").format({ async = true, lsp_fallback = true })
+				require("conform").format({ async = true }, function(err)
+					if not err then
+						local mode = vim.api.nvim_get_mode().mode
+						if vim.startswith(string.lower(mode), "v") then
+							vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
+						end
+					end
+				end)
 			end,
 			mode = "",
 			desc = "Format buffer",
@@ -28,6 +35,9 @@ return {
 			typescriptreact = { "prettierd", "prettier", stop_after_first = true },
 			vue = { "prettierd", "prettier", stop_after_first = true },
 			yaml = { "prettierd", "prettier", stop_after_first = true },
+		},
+		default_format_opts = {
+			lsp_format = "fallback",
 		},
 	},
 }
