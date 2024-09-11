6811607fac5e49c2ee36a369dfaae4c583b71be1
Oleh Aleinyk <oleg.aleynik@gmail.com>
2024-09-11 19:57:14 +0300

    LSP: rename tsserver -> ts_ls

--- lua/oaleinyk/lazy/lsp.lua
+++ lua/oaleinyk/lazy/lsp.lua
@@ -45,7 +45,7 @@ return {
 				"lua_ls",
 				"rust_analyzer",
 				"tailwindcss",
-				"tsserver",
+				"ts_ls",
 				"volar",
 				"yamlls",
 				"zls",
@@ -71,8 +71,8 @@ return {
 					})
 				end,
 
-				["tsserver"] = function()
-					lspconfig["tsserver"].setup({
+				["ts_ls"] = function()
+					lspconfig["ts_ls"].setup({
 						init_options = {
 							plugins = {
 								{
