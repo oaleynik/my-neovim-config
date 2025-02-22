780888fd488d10ebd996bffabc6c40de4ea93b97
Oleh Aleinyk <oaleynik@users.noreply.github.com>
2025-02-22 16:05:20 +0200

    Whichkey: increase delay and change style

--- lua/oaleinyk/lazy/whichkey.lua
+++ lua/oaleinyk/lazy/whichkey.lua
@@ -2,9 +2,10 @@ return {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
 	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
+		preset = "helix",
+		delay = function(ctx)
+			return ctx.plugin and 0 or 500
+		end,
 	},
 	keys = {
 		{
