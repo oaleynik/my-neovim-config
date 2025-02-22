f2729cde1dd852ca0f2c8d863619529a0266f1c3
Oleh Aleinyk <oaleynik@users.noreply.github.com>
2025-02-22 16:09:00 +0200

    Snacks: add base pickers and plugins

--- lua/oaleinyk/lazy/snacks.lua
+++ lua/oaleinyk/lazy/snacks.lua
@@ -1,75 +1,92 @@
 return {
-	"folke/snacks.nvim",
-	priority = 1001,
-	lazy = false,
-	---@type snacks.Config
-	opts = {
-		lazygit = {},
-		notifier = {
-			enabled = true,
-			timeout = 3000,
-		},
-		statuscolumn = {},
-		gitbrowse = {},
-		styles = {},
-	},
+  "folke/snacks.nvim",
+  priority = 1001,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    bigfile = { enabled = true },
+    explorer = { enabled = true },
+    input = { enabled = true },
+    notifier = { enabled = true, timeout = 3000 },
+    picker = { enabled = true },
+    quickfile = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+  },
 
-	keys = {
-		{
-			"<leader>gg",
-			function()
-				Snacks.lazygit()
-			end,
-			desc = "Open LazyGit",
-		},
-		{
-			"<leader>gB",
-			function()
-				Snacks.gitbrowse()
-			end,
-			desc = "Git Browse (Open)",
-			mode = { "n", "v" },
-		},
-		{
-			"<leader>gY",
-			function()
-				Snacks.gitbrowse({
-					open = function(url)
-						vim.fn.setreg("+", url)
-					end,
-					notify = false,
-				})
-			end,
-			desc = "Git Browse (Copy)",
-			mode = { "n", "x" },
-		},
-		{
-			"<leader>n",
-			function()
-				Snacks.notifier.show_history()
-			end,
-			desc = "Notification History",
-		},
-		{
-			"<leader>un",
-			function()
-				Snacks.notifier.hide()
-			end,
-			desc = "Dismiss All Notifications",
-		},
-		{
-			"<leader>bd",
-			function()
-				Snacks.bufdelete()
-			end,
-			desc = "Delete Buffer",
-		},
-		{
-			"<leader>bo",
-			function()
-				Snacks.bufdelete.other()
-			end,
-			desc = "Delete Other Buffers",
-		},
-	},
+  keys = {
+    --stylua: ignore start
+
+    -- Top Pickers & Explorer
+    { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
+    { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
+    { "<leader>;", function() Snacks.picker.files() end, desc = "Find Files" },
+    { "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
+    { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+
+    -- Find
+    { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+    { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+    { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
+    { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
+    { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
+    { "<leader>fs", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+
+    -- Git
+    { "<leader>gg", function() Snacks.lazygit() end, desc = "Open LazyGit" },
+    { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
+    { "<leader>gl", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
+    { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
+    { "<leader>gL", function() Snacks.picker.git_log() end, desc = "Git Log" },
+    { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
+    { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
+    { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
+    { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse (Open)", mode = { "n", "v" } },
+    {
+      "<leader>gY",
+      function()
+        local opts = Snacks.config.get("gitbrowse", {}, {
+          open = function(url) vim.fn.setreg("+", url) end,
+          notify = false
+        })
+        Snacks.gitbrowse(opts)
+      end,
+      desc = "Git Browse (Copy)",
+      mode = { "n", "x" },
+    },
+
+    -- Grep
+    { "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
+    { "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
+    { "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "x" } },
+
+    -- Search
+    { '<leader>s"', function() Snacks.picker.registers() end, desc = "Registers" },
+    { "<leader>sc", function() Snacks.picker.command_history() end, desc = "Command History" },
+    { "<leader>sC", function() Snacks.picker.commands() end, desc = "Commands" },
+    { "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
+    { "<leader>sD", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
+    { "<leader>sh", function() Snacks.picker.help() end, desc = "Help Pages" },
+    { "<leader>sH", function() Snacks.picker.highlights() end, desc = "Highlights" },
+    { "<leader>si", function() Snacks.picker.icons() end, desc = "Icons" },
+    { "<leader>sj", function() Snacks.picker.jumps() end, desc = "Jumps" },
+    { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
+    { "<leader>sl", function() Snacks.picker.loclist() end, desc = "Location List" },
+    { "<leader>sm", function() Snacks.picker.marks() end, desc = "Marks" },
+    { "<leader>sM", function() Snacks.picker.man() end, desc = "Man Pages" },
+    { "<leader>sp", function() Snacks.picker.lazy() end, desc = "Search for Plugin Spec" },
+    { "<leader>sq", function() Snacks.picker.qflist() end, desc = "Quickfix List" },
+    { "<leader>sR", function() Snacks.picker.resume() end, desc = "Resume" },
+    { "<leader>su", function() Snacks.picker.undo() end, desc = "Undo History" },
+    { "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
+
+    -- Notifications
+    { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
+
+    -- Buffers
+    { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
+    { "<leader>bo", function() Snacks.bufdelete.other() end, desc = "Delete Other Buffers" },
+
+    --stylua: ignore end
+  },
 }
