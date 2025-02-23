0899da35e0725c3d96cc873cced8659eba281264
Oleh Aleinyk <oaleynik@users.noreply.github.com>
2025-02-23 22:10:29 +0200

    Snacks: drop unnecessary searches

--- lua/oaleinyk/lazy/snacks.lua
+++ lua/oaleinyk/lazy/snacks.lua
@@ -1,6 +1,6 @@
 return {
 	"folke/snacks.nvim",
-	priority = 1001,
+	priority = 1000,
 	lazy = false,
 	---@type snacks.Config
 	opts = {
@@ -73,18 +73,14 @@ return {
 
 		-- Search
 		{ '<leader>s"', function() Snacks.picker.registers() end, desc = "Registers" },
-		{ "<leader>sc", function() Snacks.picker.command_history() end, desc = "Command History" },
-		{ "<leader>sC", function() Snacks.picker.commands() end, desc = "Commands" },
-		{ "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
-		{ "<leader>sD", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
+		{ "<leader>sc", function() Snacks.picker.commands() end, desc = "Commands" },
+		{ "<leader>sd", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
+		{ "<leader>sD", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
 		{ "<leader>sh", function() Snacks.picker.help() end, desc = "Help Pages" },
-		{ "<leader>sH", function() Snacks.picker.highlights() end, desc = "Highlights" },
 		{ "<leader>si", function() Snacks.picker.icons() end, desc = "Icons" },
-		{ "<leader>sj", function() Snacks.picker.jumps() end, desc = "Jumps" },
 		{ "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
 		{ "<leader>sl", function() Snacks.picker.loclist() end, desc = "Location List" },
 		{ "<leader>sm", function() Snacks.picker.marks() end, desc = "Marks" },
-		{ "<leader>sM", function() Snacks.picker.man() end, desc = "Man Pages" },
 		{ "<leader>sp", function() Snacks.picker.lazy() end, desc = "Search for Plugin Spec" },
 		{ "<leader>sq", function() Snacks.picker.qflist() end, desc = "Quickfix List" },
 		{ "<leader>su", function() Snacks.picker.undo() end, desc = "Undo History" },
