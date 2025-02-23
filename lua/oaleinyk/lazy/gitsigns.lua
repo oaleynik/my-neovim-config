31ae2a0335ff10b7be8728c5213766b8136be6bc
Oleh Aleinyk <oaleynik@users.noreply.github.com>
2025-02-23 19:58:20 +0200

    Gitsigns: update keymaps to align with snacks tools

--- lua/oaleinyk/lazy/gitsigns.lua
+++ lua/oaleinyk/lazy/gitsigns.lua
@@ -11,10 +11,8 @@ return {
 			on_attach = function(bufnr)
 				local opts = { noremap = true, silent = true }
 
-				vim.api.nvim_buf_set_keymap(bufnr, "n", "]h", '<cmd>lua require"gitsigns".next_hunk()<CR>', opts)
-				vim.api.nvim_buf_set_keymap(bufnr, "n", "[h", '<cmd>lua require"gitsigns".prev_hunk()<CR>', opts)
-				vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ph", '<cmd>lua require"gitsigns".preview_hunk()<CR>', opts)
-				vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rh", '<cmd>lua require"gitsigns".reset_hunk()<CR>', opts)
+				vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gp", '<cmd>lua require"gitsigns".preview_hunk_inline()<CR>', opts)
+				vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gr", '<cmd>lua require"gitsigns".reset_hunk()<CR>', opts)
 			end,
 		})
 	end,
