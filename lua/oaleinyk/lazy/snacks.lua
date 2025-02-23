f194aeca8e069fbafc465d2df3309c1048629767
Oleh Aleinyk <oaleynik@users.noreply.github.com>
2025-02-23 22:11:13 +0200

    Snacks: setup toggles and hook LspProgress to notifications

--- lua/oaleinyk/lazy/snacks.lua
+++ lua/oaleinyk/lazy/snacks.lua
@@ -94,4 +94,68 @@ return {
 		{ "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
 		{ "<leader>bo", function() Snacks.bufdelete.other() end, desc = "Delete Other Buffers" },
 	},
+
+	init = function()
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "VeryLazy",
+			callback = function()
+				_G.dd = function(...) Snacks.debug.inspect(...) end
+				_G.bt = function() Snacks.debug.backtrace() end
+				vim.print = _G.dd
+
+				Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+				Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+				Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+				Snacks.toggle.line_number():map("<leader>ul")
+				Snacks.toggle.treesitter():map("<leader>uT")
+				Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
+				Snacks.toggle.inlay_hints():map("<leader>uh")
+				Snacks.toggle.indent():map("<leader>ug")
+				Snacks.toggle.dim():map("<leader>ud")
+				Snacks.toggle
+					.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
+					:map("<leader>uc")
+			end,
+		})
+
+		---@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
+		local progress = vim.defaulttable()
+		vim.api.nvim_create_autocmd("LspProgress", {
+			---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+			callback = function(ev)
+				local client = vim.lsp.get_client_by_id(ev.data.client_id)
+				local value = ev.data.params.value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
+				if not client or type(value) ~= "table" then return end
+				local p = progress[client.id]
+
+				for i = 1, #p + 1 do
+					if i == #p + 1 or p[i].token == ev.data.params.token then
+						p[i] = {
+							token = ev.data.params.token,
+							msg = ("[%3d%%] %s%s"):format(
+								value.kind == "end" and 100 or value.percentage or 100,
+								value.title or "",
+								value.message and (" **%s**"):format(value.message) or ""
+							),
+							done = value.kind == "end",
+						}
+						break
+					end
+				end
+
+				local msg = {} ---@type string[]
+				progress[client.id] = vim.tbl_filter(function(v) return table.insert(msg, v.msg) or not v.done end, p)
+
+				local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+				vim.notify(table.concat(msg, "\n"), "info", {
+					id = "lsp_progress",
+					title = client.name,
+					opts = function(notif)
+						notif.icon = #progress[client.id] == 0 and " "
+							or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+					end,
+				})
+			end,
+		})
+	end,
 }
