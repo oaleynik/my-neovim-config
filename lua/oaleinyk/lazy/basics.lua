a6a68578d4f8664f9adfa527e14c7ed9c5ad8241
Oleh Aleinyk <oaleynik@users.noreply.github.com>
2025-02-23 22:47:45 +0200

    Mini: add mini.comment and delete Comment.nvim

--- lua/oaleinyk/lazy/basics.lua
+++ lua/oaleinyk/lazy/basics.lua
@@ -27,18 +27,6 @@ return {
 	},
 
 	{
-		"numToStr/Comment.nvim",
-
-		dependencies = {
-			"JoosepAlviste/nvim-ts-context-commentstring",
-		},
-
-		config = function()
-			require("Comment").setup()
-		end,
-	},
-
-	{
 		"norcalli/nvim-colorizer.lua",
 
 		config = function()
--- lua/oaleinyk/lazy/mini.lua
+++ lua/oaleinyk/lazy/mini.lua
@@ -3,6 +3,7 @@ return {
 		"echasnovski/mini.nvim",
 		version = false,
 		init = function()
+			require("mini.comment").setup()
 			require("mini.move").setup()
 			require("mini.splitjoin").setup()
 			require("mini.surround").setup()
