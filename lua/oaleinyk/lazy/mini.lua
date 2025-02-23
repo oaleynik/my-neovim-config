1d6368d1ff88e88f4b9414b8dee636b993dd257e
Oleh Aleinyk <oaleynik@users.noreply.github.com>
2025-02-23 22:41:31 +0200

    Mini: add mini.move to handle lines movement

--- lua/oaleinyk/lazy/mini.lua
+++ lua/oaleinyk/lazy/mini.lua
@@ -3,6 +3,7 @@ return {
 		"echasnovski/mini.nvim",
 		version = false,
 		init = function()
+			require("mini.move").setup()
 			require("mini.splitjoin").setup()
 			require("mini.surround").setup()
 		end,
