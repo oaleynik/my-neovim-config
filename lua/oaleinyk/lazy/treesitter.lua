e464610d05d15b186b8b7221822eb32188908c31
Oleh Aleinyk <oaleynik@users.noreply.github.com>
2025-02-26 00:52:52 +0200

    Treesitter: update config

--- lua/oaleinyk/lazy/treesitter.lua
+++ lua/oaleinyk/lazy/treesitter.lua
@@ -1,19 +1,12 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		"nvim-treesitter/playground",
-	},
-
-	build = ":TSUpdate",
-
-	config = function()
-		local treesitter = require("nvim-treesitter.configs")
-
-		treesitter.setup({
+	{
+		"nvim-treesitter/nvim-treesitter",
+		version = false,
+		build = ":TSUpdate",
+		lazy = vim.fn.argc(-1) == 0,
+		cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+		opts = {
 			ensure_installed = {
-				-- 'comment', -- comments are slowing down TS bigtime, so disable for now
 				"css",
 				"gitcommit",
 				"gitignore",
@@ -32,42 +25,8 @@ return {
 				"vimdoc",
 				"vue",
 			},
-			ignore_install = {},
-			auto_install = true,
-			sync_install = false,
-			autopath = {
-				enable = true,
-			},
-			highlight = {
-				enable = true,
-			},
-			indent = {
-				enable = true,
-			},
-			incremental_selection = {
-				enable = true,
-			},
-			playground = {
-				enable = true,
-			},
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true,
-					keymaps = {
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@class.outer",
-						["ic"] = "@class.inner",
-					},
-				},
-				swap = {
-					enable = true,
-				},
-				move = {
-					enable = true,
-				},
-			},
-		})
-	end,
+			highlight = { enable = true },
+			indent = { enable = true },
+		},
+	},
 }
