48de04114f9408fdddb52e03e065edb9641d0362
Oleh Aleinyk <oaleynik@users.noreply.github.com>
2025-03-01 22:40:32 +0200

    Mini: add pairs module

--- lua/oaleinyk/lazy/mini.lua
+++ lua/oaleinyk/lazy/mini.lua
@@ -7,6 +7,7 @@ return {
 			require("mini.align").setup()
 			require("mini.comment").setup()
 			require("mini.move").setup()
+			require("mini.pairs").setup()
 			require("mini.splitjoin").setup()
 			require("mini.surround").setup()
 		end,
