1f7a4ffdc6fcd4fdf52b889418493642cccc55dd
Oleh Aleinyk <oaleynik@users.noreply.github.com>
2025-03-06 16:06:49 +0200

    Theme: tokyonight moon

--- lua/oaleinyk/lazy/themes.lua
+++ lua/oaleinyk/lazy/themes.lua
@@ -12,7 +12,8 @@ return {
 
 	{
 		"folke/tokyonight.nvim",
-		lazy = true,
+		lazy = false,
+		priority = 1000,
 	},
 
 	{
@@ -73,8 +74,7 @@ return {
 
 	{
 		"sainnhe/gruvbox-material",
-		lazy = false,
-		priority = 1000,
+		lazy = true,
 		config = function()
 			vim.g.gruvbox_material_background = "hard"
 			vim.g.gruvbox_material_better_performance = 1
