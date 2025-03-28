47e2c00e1ed18cbbb7bf1fa2b0fc72f0f359033c
Oleh Aleinyk <oaleynik@users.noreply.github.com>
2025-03-28 16:19:51 +0200

    LSP: drop keymaps that have default alternatives

--- lua/oaleinyk/lazy/lsp.lua
+++ lua/oaleinyk/lazy/lsp.lua
@@ -206,12 +206,6 @@ return {
 					local opts = { buffer = ev.buf }
 					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-					vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 				end,
 			})
 
