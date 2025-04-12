3b6ac093a4a5d45bc0ae773c7c639f6458a3c87c
Oleh Aleinyk <oaleynik@users.noreply.github.com>
2025-04-12 23:58:03 +0300

    Gitsigns: configure hunks navigation

--- lua/oaleinyk/lazy/gitsigns.lua
+++ lua/oaleinyk/lazy/gitsigns.lua
@@ -3,16 +3,20 @@ return {
 
 	config = function()
 		local status, gitsigns = pcall(require, "gitsigns")
-		if not status then
-			return
-		end
+		if not status then return end
 
 		gitsigns.setup({
 			on_attach = function(bufnr)
 				local opts = { noremap = true, silent = true }
 
+				vim.api.nvim_buf_set_keymap(bufnr, "n", "]H", '<cmd>lua require"gitsigns".nav_hunk("last")<CR>', opts)
+				vim.api.nvim_buf_set_keymap(bufnr, "n", "]h", '<cmd>lua require"gitsigns".nav_hunk("next")<CR>', opts)
+				vim.api.nvim_buf_set_keymap(bufnr, "n", "[h", '<cmd>lua require"gitsigns".nav_hunk("prev")<CR>', opts)
+				vim.api.nvim_buf_set_keymap(bufnr, "n", "[H", '<cmd>lua require"gitsigns".nav_hunk("first")<CR>', opts)
 				vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gp", '<cmd>lua require"gitsigns".preview_hunk_inline()<CR>', opts)
 				vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gr", '<cmd>lua require"gitsigns".reset_hunk()<CR>', opts)
+				vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gb", '<cmd>lua require"gitsigns".blame_line()<CR>', opts)
+				vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gB", '<cmd>lua require"gitsigns".blame()<CR>', opts)
 			end,
 		})
 	end,
