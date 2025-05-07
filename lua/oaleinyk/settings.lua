5bc5379d5f7ce87e35c6f203e8b202aa5e827766
Oleh Aleinyk <oaleynik@users.noreply.github.com>
2025-05-08 02:28:36 +0300

    Settings: rewrite autocommands with lua API

--- lua/oaleinyk/settings.lua
+++ lua/oaleinyk/settings.lua
@@ -46,12 +46,12 @@ vim.o.matchtime = 2     -- delay before showing matching paren
 -- White characters
 vim.o.autoindent = true
 vim.o.smartindent = true
-vim.o.tabstop = 2            -- 1 tab = 2 spaces
-vim.o.shiftwidth = 2         -- indent rule
+vim.o.tabstop = 2    -- 1 tab = 2 spaces
+vim.o.shiftwidth = 2 -- indent rule
 vim.o.shiftround = true
 vim.o.formatoptions =
-"qnj1"                       -- q - comment formatting; n - numbered lists; j - remove comment when joining lines; 1 - don't break after one-letter word
-vim.o.expandtab = true       -- expand tab to spaces
+"qnj1"                 -- q - comment formatting; n - numbered lists; j - remove comment when joining lines; 1 - don't break after one-letter word
+vim.o.expandtab = true -- expand tab to spaces
 vim.o.virtualedit = "onemore"
 
 -- Backup files
@@ -75,24 +75,39 @@ vim.o.wildignore =
 
 vim.o.winborder = "rounded"
 
--- Only show cursorline in the current window and in normal mode
-vim.cmd([[
-  augroup cline
-  au!
-  au WinLeave * set nocursorline
-  au WinEnter * set cursorline
-  au InsertEnter * set nocursorline
-  au InsertLeave * set cursorline
-  augroup END
-]])
+-- Show cursorline only in current window and in normal mode
+local cline = vim.api.nvim_create_augroup("Cursorline", { clear = true })
+vim.api.nvim_create_autocmd(
+  { "WinLeave", "InsertEnter" },
+  {
+    group = cline,
+    pattern = "*",
+    callback = function()
+      vim.wo.cursorline = false
+    end,
+  }
+)
+vim.api.nvim_create_autocmd(
+  { "WinEnter", "InsertLeave" },
+  {
+    group = cline,
+    pattern = "*",
+    callback = function()
+      vim.wo.cursorline = true
+    end,
+  }
+)
 
-vim.cmd([[
-  autocmd ColorScheme * highlight! link SignColumn LineNr
-]])
-
-vim.cmd([[
-  autocmd BufReadPost *
-  \ if line("'\"") >= 1 && line("'\"") <= line("$")
-  \ |   exe "normal! g`\""
-  \ | endif
-]])
+-- Restore cursor position when opening files
+vim.api.nvim_create_autocmd(
+  { "BufReadPost" },
+  {
+    pattern = "*",
+    callback = function()
+      local last_pos = vim.fn.line("'\"")
+      if last_pos > 0 and last_pos <= vim.fn.line("$") then
+        vim.cmd("normal! g`\"")
+      end
+    end,
+  }
+)
