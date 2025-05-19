11d64101343b44250a01d95210fb24d9ee45e190
Oleh Aleinyk <oaleynik@users.noreply.github.com>
2025-05-19 16:02:10 +0300

    Lsp: properly configure ts_ls and vls

--- lsp/ts_ls.lua
+++ lsp/ts_ls.lua
@@ -1,13 +1,17 @@
-local install_dir = vim.fn.expand "$MASON/packages/vue-language-server" .. "/node_modules/@vue/language-server"
 return {
   init_options = {
     plugins = {
       {
         name = "@vue/typescript-plugin",
-        location = install_dir,
-        languages = { "vue" },
+        location = vim.fn.expand "$MASON/packages/vue-language-server" .. "/node_modules/@vue/language-server",
+        languages = { "javascript", "typescript", "vue" },
       },
     },
+  },
+  filetypes = {
+    "javascript",
+    "typescript",
+    "vue",
   },
   settings = {
     typescript = {
---
+++ lsp/volar.lua
@@ -0,0 +1,16 @@
+return {
+  init_options = {
+    typescript = {
+      tsdk = vim.fn.expand "$MASON/packages/typescript-language-server" .. "/node_modules/typescript/lib",
+    }
+  },
+  before_init = function(_, config)
+    local lib_path = vim.fs.find('node_modules/typescript/lib', {
+      path = config.root_dir,
+      upward = true,
+    })[1]
+    if lib_path then
+      config.init_options.typescript.tsdk = lib_path
+    end
+  end
+}
