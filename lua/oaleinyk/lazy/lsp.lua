838dee638bf014b623251cfd71abaac7916963e8
Oleh Aleinyk <oaleynik@users.noreply.github.com>
2025-05-21 20:10:25 +0300

    Lsp: disable virtual lines

--- lua/oaleinyk/lazy/lsp.lua
+++ lua/oaleinyk/lazy/lsp.lua
@@ -47,10 +47,6 @@ return {
         },
       })
 
-      vim.diagnostic.config({
-        virtual_lines = true,
-      })
-
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("UserLspConfig", {}),
         callback = function(args)
