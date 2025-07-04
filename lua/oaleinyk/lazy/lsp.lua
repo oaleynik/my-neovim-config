c981962b04f27d81d3eca7bf8b13f8a01d71ef67
Oleh Aleinyk <oaleynik@users.noreply.github.com>
2025-07-04 16:35:02 +0300

    LSP: autoformat go file on save

--- lua/oaleinyk/lazy/lsp.lua
+++ lua/oaleinyk/lazy/lsp.lua
@@ -53,6 +53,24 @@ return {
 				return function() vim.diagnostic.jump({ count = count, severity = { min = severity } }) end
 			end
 
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = "*.go",
+				callback = function()
+					local params = vim.lsp.util.make_range_params(vim.api.nvim_get_current_win(), "utf-16")
+
+					local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+					for cid, res in pairs(result or {}) do
+						for _, r in pairs(res.result or {}) do
+							if r.edit then
+								local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+								vim.lsp.util.apply_workspace_edit(r.edit, enc)
+							end
+						end
+					end
+					vim.lsp.buf.format({ async = false })
+				end,
+			})
+
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 				callback = function(args)
