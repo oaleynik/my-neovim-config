9b56a6cba7d8b544c2d39049199e9bf12926fa80
Oleh Aleinyk <oaleynik@users.noreply.github.com>
2025-09-04 16:53:49 +0300

    LSP: don't apply all edit code actions in go files

--- lua/oaleinyk/lazy/lsp.lua
+++ lua/oaleinyk/lazy/lsp.lua
@@ -62,8 +62,10 @@ return {
 					for cid, res in pairs(result or {}) do
 						for _, r in pairs(res.result or {}) do
 							if r.edit then
-								local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-								vim.lsp.util.apply_workspace_edit(r.edit, enc)
+								if r.kind == "source.organizeImports" then
+									local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+									vim.lsp.util.apply_workspace_edit(r.edit, enc)
+								end
 							end
 						end
 					end
