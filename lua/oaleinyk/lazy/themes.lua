a793b1f8a270bb2d09c5caca78b9cebf14570722
Oleh Aleinyk <oaleynik@users.noreply.github.com>
2025-09-26 17:23:52 +0300

    Theme: use gruvbox material

--- lua/oaleinyk/lazy/themes.lua
+++ lua/oaleinyk/lazy/themes.lua
@@ -12,8 +12,7 @@ return {
 
 	{
 		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
+		lazy = true,
 	},
 
 	{
@@ -74,7 +73,8 @@ return {
 
 	{
 		"sainnhe/gruvbox-material",
-		lazy = true,
+		lazy = false,
+		priority = 1000,
 		config = function()
 			vim.g.gruvbox_material_background = "hard"
 			vim.g.gruvbox_material_better_performance = 1
