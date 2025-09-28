3d3367742e3f162ccfe7da5b34ed33d4a62c57f3
Oleh Aleinyk <oaleynik@users.noreply.github.com>
2025-09-28 18:42:55 +0300

    Lualine: adjust sections for better visibility

--- lua/oaleinyk/lazy/lualine.lua
+++ lua/oaleinyk/lazy/lualine.lua
@@ -9,15 +9,36 @@ return {
 			options = {
 				icons_enabled = true,
 				theme = "auto",
-				component_separators = { left = "\\", right = "/" },
-				section_separators = { left = "", right = "" },
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
 				disabled_filetypes = {},
 				always_divide_middle = true,
 				globalstatus = false,
 			},
 			sections = {
-				lualine_a = { { "branch", icon = "" }, "diff", "diagnostics" },
-        lualine_b = {},
+				lualine_a = {
+					{ "branch", icon = "" },
+					{
+						"diff",
+						diff_color = {
+							added = { fg = Snacks.util.color("DiffAdd", "bg") },
+							modified = { fg = Snacks.util.color("DiffChange", "bg") },
+							removed = { fg = Snacks.util.color("DiffDelete", "bg") },
+						},
+					},
+					{
+						"diagnostics",
+						{
+							diagnostics_color = {
+								error = { fg = Snacks.util.color("DiagnosticError") },
+								warn = { fg = Snacks.util.color("DiagnosticWarn") },
+								info = { fg = Snacks.util.color("DiagnosticInfo") },
+								hint = { fg = Snacks.util.color("DiagnosticHint") },
+							},
+						},
+					},
+				},
+				lualine_b = {},
 				lualine_c = {},
 				lualine_x = { "encoding", "fileformat", "filetype" },
 				lualine_y = { "progress" },
