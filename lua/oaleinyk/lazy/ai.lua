7e74f355728d007c05151a9e002fc19e4cd5a4e4
Oleh Aleinyk <oaleynik@users.noreply.github.com>
2025-10-05 00:45:51 +0300

    AI: configure sidekick

--- lua/oaleinyk/lazy/ai.lua
+++ lua/oaleinyk/lazy/ai.lua
@@ -1,14 +1,69 @@
 return {
-	{
-		"github/copilot.vim",
-		event = "VimEnter",
-		init = function()
-			vim.g.copilot_no_tab_map = false
-			vim.g.copilot_workspace_folders = { vim.fn.getcwd() }
-		end,
-		keys = {
-			{ "<Tab>", "<Plug>(copilot-accept-line)", { expr = true, silent = true, mode = "i" } },
-			{ "<M-]>", "<Plug>(copilot-next)", { expr = true, silent = true, mode = "i" } },
+	"folke/sidekick.nvim",
+	opts = {
+		cli = {
+			mux = {
+				backend = "tmux",
+				enabled = true,
+			},
+			tools = {
+				amp = { cmd = { "amp" } },
+			},
+		},
+	},
+	keys = {
+		{
+			"<tab>",
+			function()
+				if not require("sidekick").nes_jump_or_apply() then return "<Tab>" end
+			end,
+			expr = true,
+			desc = "Goto/Apply Next Edit Suggestion",
+		},
+		{
+			"<leader>as",
+			function() require("sidekick.cli").select({ filter = { installed = true } }) end,
+			desc = "Select CLI",
+		},
+		{
+			"<leader>ac",
+			function() require("sidekick.cli").toggle({ name = "copilot", focus = true }) end,
+			desc = "Sidekick Toggle Copilot",
+		},
+		{
+			"<leader>aa",
+			function() require("sidekick.cli").toggle({ name = "amp", focus = true }) end,
+			desc = "Sidekick Toggle Amp",
+		},
+		{
+			"<leader>at",
+			function() require("sidekick.cli").send({ msg = "{this}" }) end,
+			mode = { "x", "n" },
+			desc = "Send This",
+		},
+		{
+			"<leader>av",
+			function() require("sidekick.cli").send({ msg = "{selection}" }) end,
+			mode = { "x" },
+			desc = "Send Visual Selection",
+		},
+		{
+			"<leader>ap",
+			function() require("sidekick.cli").prompt() end,
+			mode = { "n", "x" },
+			desc = "Sidekick Select Prompt",
+		},
+		{
+			"<leader>at",
+			function() require("sidekick.cli").toggle() end,
+			mode = { "n", "x" },
+			desc = "Sidekick Toggle",
+		},
+		{
+			"<c-.>",
+			function() require("sidekick.cli").focus() end,
+			mode = { "n", "x", "i", "t" },
+			desc = "Sidekick Switch Focus",
 		},
 	},
 }
--- lua/oaleinyk/lazy/blink.lua
+++ lua/oaleinyk/lazy/blink.lua
@@ -3,7 +3,14 @@ return {
 	dependencies = { "rafamadriz/friendly-snippets" },
 	version = "1.*",
 	opts = {
-		keymap = { preset = "default" },
+		keymap = {
+			preset = "default",
+			["<Tab>"] = {
+				"snippet_forward",
+				function() return require("sidekick").nes_jump_or_apply() end,
+				"fallback",
+			},
+		},
 		appearance = {
 			use_nvim_cmp_as_default = true,
 			nerd_font_variant = "mono",
